a772df69f01d2fb26a3362306e5d475c1c70553d
mlyman1234 <mlyman1@my.westga.edu>
2025-09-10 21:08:07 -0400

    Designing and Implementing a GUI for the Bill System
    1) Integrated the Bill code from Lab2 into JavaFX
    2) Designed GUI using Scene Builder by adding TextFields for the name and amount variables, and a TextArea for the BillView output.
    3) I added functionality to the GUI so that it can create a BillItem using the name and amount TextFields, add the BillItem to a list, then generate a string using the getText method from BillView and displaying it in the output TextArea.

---
+++ code/Bill/src/edu/westga/cs1302/bill/model/Bill.java
@@ -0,0 +1,69 @@
+package edu.westga.cs1302.bill.model;
+
+import java.util.ArrayList;
+
+/** Stores information for a bill.
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class Bill {
+	public static final double TIP_RATE = 0.2;
+	public static final double TAX_RATE = 0.1;
+	
+	private BillItem[] items;
+	private int size;
+	
+	/** Create a new empty Bill
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 */
+	public Bill() {
+		this.items = new BillItem[5];
+		this.size = 0;
+	}
+	
+	/** Adds the item to the bill
+	 * 
+	 * @precondition item != null
+	 * @postcondition item is added to the list of items in the bill
+	 * 
+	 * @param item the item to be added to the bill
+	 * 
+	 * @throws IndexOutOfBoundsException if adding new item when array is full
+	 */
+	public void addItem(BillItem item) throws IndexOutOfBoundsException {
+		if (item == null) {
+			throw new IllegalArgumentException("item must not be null.");
+		}
+		try {
+		this.items[this.size] = item;
+		this.size++;
+		} catch (IndexOutOfBoundsException error) {
+			BillItem[] newItems = new BillItem[this.size*2];
+			int newSize = 0;
+			for(BillItem currentItem : this.items) {
+				newItems[newSize] = currentItem;
+				newSize++;
+			}
+			newItems[newSize] = item;
+			newSize++;
+			this.items = newItems;
+			this.size = newSize;
+		}
+	}
+
+	/** Returns the list of items in the bill
+	 * 
+	 * @precondition none
+	 * @postconition none
+	 * 
+	 * @return the list of items in the bill
+	 */
+	public BillItem[] getItems() {
+		return this.items;
+	}
+
+}
--- code/JavaFX Sample Starter (5)/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ code/JavaFX Sample Starter (5)/JavaFX Sample Starter/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -1,18 +1,43 @@
 package edu.westga.cs1302.lab3.views;
 
+import edu.westga.cs1302.lab3.model.Bill;
+import edu.westga.cs1302.lab3.model.BillItem;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 
 /**
- * Controller class for drawing various things to our canvas window.
+ * This generates a window for the program to operate.
  * 
  * @author CS 1302
  * @version Fall 2025
  */
 public class MainWindow {
+
+    @FXML
+    private TextField amountInput;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private TextArea output;
     
-    /**
-     * Perform any needed initialization of UI components and underlying objects.
-     */
-    public void initialize() {
-    	
+    private Bill bill;
+
+    @FXML
+    void addItem(ActionEvent event) {
+    	BillView text = new BillView();
+    	BillItem item = new BillItem(this.nameInput.getText(), Double.parseDouble(this.amountInput.getText()));
+    	this.bill.addItem(item);
+    	var receipt = text.getText(this.bill);
+    	this.output.setText(receipt);
+    }
+    
+    @FXML
+    void initialize() {
+    	this.bill = new Bill();
     }
 }
