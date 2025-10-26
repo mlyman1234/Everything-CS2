749c1ccd2836d890c9f91e2925d85fa74850f963
mlyman1234 <mlyman1@my.westga.edu>
2025-10-26 11:25:55 -0400

    1 – Re-sort When Adding or Updating Tasks A – Connect UI to Business Logic for Re-sorting Tasks Update MainWindow to resort tasks whenever a task is added or updated in any way.

--- code/TaskTracker/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/TaskComparatorAscending.java
+++ code/TaskTracker/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/TaskComparatorAscending.java
@@ -4,18 +4,23 @@ import java.util.Comparator;
 
 public class TaskComparatorAscending implements Comparator<Task> {
 
-	
+	/**Compare two Tasks to Identify the correct Ascending ordering of the tasks.
+	 * 
+	 * @author CS1302
+	 * @version fall 2025
+	 * 
+	 */
 	@Override
 	public int compare(Task o1, Task o2) {
 		var result = 0;
 		
-		if(o1.getPriority() == o2.getPriority()) {
+		if (o1.getPriority() == o2.getPriority()) {
 			result = 0;
 		}
-		else if(o1.taskPriorityToInt() > o2.taskPriorityToInt()) {
+		else if (o1.taskPriorityToInt() > o2.taskPriorityToInt()) {
 			result = 1;
 		}
-		else if(o1.taskPriorityToInt() < o2.taskPriorityToInt()) {
+		else if (o1.taskPriorityToInt() < o2.taskPriorityToInt()) {
 			result = -1;
 		}
 		return result;
--- code/TaskTracker/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/TaskComparatorDescending.java
+++ code/TaskTracker/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/TaskComparatorDescending.java
@@ -9,13 +9,13 @@ public class TaskComparatorDescending implements Comparator<Task> {
 	public int compare(Task o1, Task o2) {
 		var result = 0;
 		
-		if(o1.getPriority() == o2.getPriority()) {
+		if (o1.getPriority() == o2.getPriority()) {
 			result = 0;
 		}
-		else if(o1.taskPriorityToInt() > o2.taskPriorityToInt()) {
+		else if (o1.taskPriorityToInt() > o2.taskPriorityToInt()) {
 			result = -1;
 		}
-		else if(o1.taskPriorityToInt() < o2.taskPriorityToInt()) {
+		else if (o1.taskPriorityToInt() < o2.taskPriorityToInt()) {
 			result = 1;
 		}
 		return result;
