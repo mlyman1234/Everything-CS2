d5c4db14c6587bd787b501ed780293b25ebb4850
mlyman1234 <mlyman1@my.westga.edu>
2025-11-13 00:00:38 -0500

    Implement Input Validation
    You need to implement the appropriate change listener(s) to validate the input fields as appropriate, and you must use a regular expression to define your expectations for at least one input field.

--- code/starterL7/Lab 7 Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/starterL7/Lab 7 Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.util.converter.NumberStringConverter;
+
 
 /** Codebehind for the MainWindow of the Application.
  * 
---
+++ code/starterL8/L8 - Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -0,0 +1,60 @@
+package edu.westga.cs1302.password_generator.view;
+
+import edu.westga.cs1302.password_generator.viewmodel.ViewModel;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/** Codebehind for the MainWindow of the Application.
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class MainWindow {
+
+    @FXML private CheckBox mustIncludeDigits;
+    @FXML private CheckBox mustIncludeLowerCaseLetters;
+    @FXML private CheckBox mustIncludeUpperCaseLetters;
+    @FXML private TextField minimumLength;
+    @FXML private TextArea output;
+    @FXML private Label errorTextLabel;
+    @FXML private Button generatePasswordButton;
+    
+    private ViewModel vm;
+    
+    private void bindBehavior() {
+    this.minimumLength.textProperty().addListener(
+        	(observable, oldValue, newValue) -> {
+        			if (newValue.matches("[^Integer.MIN_VALUE-0]")) {
+        				this.errorTextLabel.setVisible(true);
+        			}
+        			else {
+        				this.errorTextLabel.setVisible(false);
+        			}
+        	}
+    	);
+    }
+    
+    @FXML
+    void initialize() {
+    	this.vm = new ViewModel();
+    	this.bindBehavior();
+    	this.vm.getRequireDigits().bind(this.mustIncludeDigits.selectedProperty());
+    	this.vm.getRequireLowercase().bind(this.mustIncludeLowerCaseLetters.selectedProperty());
+    	this.vm.getRequireUppercase().bind(this.mustIncludeUpperCaseLetters.selectedProperty());
+    	this.minimumLength.setText(this.vm.getMinimumLength().getValue());
+    	this.vm.getMinimumLength().bind(this.minimumLength.textProperty());
+    	
+    	this.output.textProperty().bind(this.vm.getPassword());
+    	this.errorTextLabel.textProperty().bind(this.vm.getErrorText());
+    	
+    	this.generatePasswordButton.setOnAction(
+    			(event) -> { 
+    				this.vm.generatePassword();
+    			} 
+    	);
+    }
+}
