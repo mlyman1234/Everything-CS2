75d98cfe91e5225ad6dc614dc3e03bbaaf83eb24
mlyman1234 <mlyman1@my.westga.edu>
2025-12-02 08:54:12 -0500

    Prevent duplicate contacts
    You should update the system to not allow storing a contact if there is an existing contact with the same name or phone number.

--- code/lab10Starter/Lab 10/src/test/java/mainWindowViewModel/testAddContact.java
+++ code/lab10Starter/Lab 10/src/test/java/mainWindowViewModel/testAddContact.java
@@ -1,5 +1,46 @@
 package mainWindowViewModel;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.contact_manager.model.Contact;
+import edu.westga.cs1302.contact_manager.viewmodel.MainWindowViewModel;
+
 public class testAddContact {
+	
+	
+	@Test
+	public void ifDuplicateFound() {
+		MainWindowViewModel vm = new MainWindowViewModel();
+		vm.getName().set("S");
+		vm.getPhoneNumber().set("999-9999");
+		vm.addContact();
+		vm.getName().set("S");
+		vm.getPhoneNumber().set("999-9999");
+		assertThrows(IllegalArgumentException.class, () -> {
+			vm.addContact();
+		});
+	}
+		
+		@Test
+		public void addMultiple() {
+			MainWindowViewModel vm = new MainWindowViewModel();
+			vm.getName().set("S");
+			vm.getPhoneNumber().set("999-9999");
+			vm.addContact();
+			vm.getName().set("M");
+			vm.getPhoneNumber().set("999-9599");
+			vm.addContact();
+			assertFalse(vm.getContacts().isEmpty());
+			vm.getSearchCriteria().setValue("S");
+			assertEquals(vm.findContact(), "S, 999-9999");
+			vm.getSearchCriteria().setValue("M");
+			assertEquals(vm.findContact(), "M, 999-9599");
+
+			
+	}
 
 }
--- code/lab10Starter/Lab 10/src/test/java/mainWindowViewModel/testFindContact.java
+++ code/lab10Starter/Lab 10/src/test/java/mainWindowViewModel/testFindContact.java
@@ -1,5 +1,64 @@
 package mainWindowViewModel;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.contact_manager.viewmodel.MainWindowViewModel;
+
 public class testFindContact {
 
+	@Test
+	public void ifSearchCriteriaIsNotValid() {
+		MainWindowViewModel vm = new MainWindowViewModel();
+		vm.getName().set("S");
+		vm.getPhoneNumber().set("999-9999");
+		vm.addContact();
+		assertThrows(IllegalArgumentException.class, () -> {
+			vm.getSearchCriteria().setValue("999999999");
+		});
+	}
+	
+	@Test
+	public void findingForPhoneNumber() {
+		MainWindowViewModel vm = new MainWindowViewModel();
+		vm.getName().set("S");
+		vm.getPhoneNumber().set("999-9999");
+		vm.addContact();
+		vm.getName().set("M");
+		vm.getPhoneNumber().set("999-9959");
+		vm.addContact();
+		vm.getName().set("L");
+		vm.getPhoneNumber().set("899-9999");
+		vm.addContact();
+		vm.getSearchCriteria().setValue("999-9999");
+		assertEquals(vm.findContact(), "S, 999-9999");
+		vm.getSearchCriteria().setValue("999-9959");
+		assertEquals(vm.findContact(), "M, 999-9959");
+		vm.getSearchCriteria().setValue("899-9999");
+		assertEquals(vm.findContact(), "L, 899-9999");
+
+	}
+	
+	@Test
+	public void findingForName() {
+		MainWindowViewModel vm = new MainWindowViewModel();
+		vm.getName().set("S");
+		vm.getPhoneNumber().set("999-9999");
+		vm.addContact();
+		vm.getName().set("M");
+		vm.getPhoneNumber().set("999-9959");
+		vm.addContact();
+		vm.getName().set("L");
+		vm.getPhoneNumber().set("899-9999");
+		vm.addContact();
+		vm.getSearchCriteria().setValue("S");
+		assertEquals(vm.findContact(), "S, 999-9999");
+		vm.getSearchCriteria().setValue("M");
+		assertEquals(vm.findContact(), "M, 999-9959");
+		vm.getSearchCriteria().setValue("L");
+		assertEquals(vm.findContact(), "L, 899-9999");
+
+	}
 }
