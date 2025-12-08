8d71fe109703b4d2e4f506d2b24edb56c36bae86
mlyman1234 <mlyman1@my.westga.edu>
2025-12-08 17:54:24 -0500

    B – Model for Adding Comic to a Collection
    Implement an appropriate data class to store a comic.
    
    Update the collection class to store a list of comics.

---
+++ code/L16-SampleSystem/L16 - Sample System/src/main/java/edu/westga/cs1302/investment_projector/model/ComicData.java
@@ -0,0 +1,5 @@
+package edu.westga.cs1302.investment_projector.model;
+
+public class ComicData {
+
+}
--- code/L16-SampleSystem/L16 - Sample System/src/main/java/edu/westga/cs1302/investment_projector/view/ComicWindow.java
+++ code/L16-SampleSystem/L16 - Sample System/src/main/java/edu/westga/cs1302/investment_projector/view/ComicWindow.java
@@ -16,52 +16,19 @@ import javafx.scene.control.Button;
  * @version Fall 2024
  */
 public class ComicWindow {
-	@FXML private ListView<CollectionData> collectionOfNames;
-    @FXML private TextField name;
+	@FXML private Button addingComic;
+
     @FXML private Button addingName;
-    @FXML private Button removingName;
-    @FXML private Button addingComic;
-    @FXML private Button removingComic;
-    @FXML private ListView<?> comics;
+
+    @FXML private TextField issueTextField;
+
+    @FXML private TextField nameTextField;
     
     private CollectionVM vm;
     
     @FXML
     void initalize() {
-    	this.vm = new CollectionVM();
-        this.vm.getName().bind(this.name.textProperty());
-		this.collectionOfNames.setItems(this.vm.getCollection());
-
-		this.name.textProperty().addListener((observable, oldValue, newValue) -> {
-			if (newValue.isEmpty()) {
-				this.addingName.setDisable(true);
-			} else {
-				this.addingName.setDisable(false);
-			}
-	});
-    
-    	this.addingName.setOnAction((event) -> {
-    		try {
-    			this.vm.addName();
-    		} catch (IllegalArgumentException error) {
-    			Alert alert = new Alert(AlertType.ERROR);
-    			alert.setContentText(error.getMessage());
-    			alert.showAndWait();
-    		} 
-    	});
-    	  	
-    	this.removingName.setOnAction((event) -> {
-    		try {
-    			this.vm.removeName(this.collectionOfNames.getSelectionModel().getSelectedItem());
-    		} catch (IllegalArgumentException error) {
-    			Alert alert = new Alert(AlertType.ERROR);
-    			alert.setContentText(error.getMessage());
-    			alert.showAndWait();
-    		}
-    	});
-
+    	
     }
-    
-    
 
 }
